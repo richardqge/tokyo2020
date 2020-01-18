2b7425b1bbc4767f6f270b08419039e7c37e0664
Roy Ge <rgecoder@gmail.com>
2020-01-18 10:04:40 -0800

    Heroku V1.0

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var commentRoutes = require("./routes/comments"),
   indexRoutes = require("./routes/index");
 
 // var url = process.env.DATABASEURL || "mongodb://localhost/tokyo2020_local";
-mongoose.connect("mongodb+srv://rgecoder:rgecoder@tokyo2020v1-xgatl.mongodb.net/test?retryWrites=true&w=majority");
-// mongodb+srv://rgecoder:<password>@tokyo2020v1-xgatl.mongodb.net/test?retryWrites=true&w=majority
+// mongoose.connect("mongodb+srv://rgecoder:rgecoder@tokyo2020v1-xgatl.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb://localhost/tokyo2020_local");
 
 // mongoose.connect("mongodb://localhost/tokyo2020_test2");
 
@@ -56,6 +56,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function() {
+// app.listen(process.env.PORT, process.env.IP, function() {
+app.listen(3000, function() {
   console.log("The YelpCamp Server Has Started!");
 });
