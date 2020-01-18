223dc67b9dc3da7d4664525874c0727c6a64ce69
Roy Ge <rgecoder@gmail.com>
2020-01-18 08:40:14 -0800

    changed port

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
   console.log("The YelpCamp Server Has Started!");
 });
